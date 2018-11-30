2671f5b6fd755b921510a7a52602a2db1d62bdd5
Patrick Balestra <me@patrickbalestra.com>
2018-11-30 12:38:36 +0100

    Deploy Vue for dynamic speakers loading

--- js/app.js
+++ js/app.js
@@ -26,39 +26,10 @@ new Vue({
 
             db.collection("speakers").where("isStory", "==", false).get().then((speakers) => {
                 var result = [];
-                var tmp = [];
-                speakers.forEach((s) => {
-                    if (tmp.length == 4) {
-                        result.push(tmp.slice());
-                        tmp = [];
-                    }
-                    tmp.push(s.data());
-                });
-                result.push(tmp.slice());
-                self.speakers = result;
-            });
-
-        }
-    }
-});
-
-new Vue({
-    el: '#stories',
-    data: {
-        speakers: []
-    },
-    mounted: function() {
-        this.loadData();
-    },
-    methods: {
-        loadData: function() {
-            var self = this;
-
-            db.collection("speakers").where("isStory", "==", true).get().then((speakers) => {
-                var result = [];
                 speakers.forEach((s) => {
                     result.push(s.data());
                 });
+                console.log(result);
                 self.speakers = result;
             });
 
@@ -66,72 +37,96 @@ new Vue({
     }
 });
 
-new Vue({
-    el: '#schedule',
-    data: {
-        talks: [],
-        speakers: {}
-    },
-    mounted: function() {
-        this.loadData();
-    },
-    methods: {
-        loadData: function() {
-            var self = this;
+// new Vue({
+//     el: '#stories',
+//     data: {
+//         speakers: []
+//     },
+//     mounted: function() {
+//         this.loadData();
+//     },
+//     methods: {
+//         loadData: function() {
+//             var self = this;
 
-            db.collection("speakers").get().then((speakers) => {
-                var spks = {};
-                speakers.forEach((s) => {
-                    var data = s.data();
-                    spks[data["speakerId"]] = data;
-                });
-                self.speakers = spks;
+//             db.collection("speakers").where("isStory", "==", true).get().then((speakers) => {
+//                 var result = [];
+//                 speakers.forEach((s) => {
+//                     result.push(s.data());
+//                 });
+//                 self.speakers = result;
+//             });
 
-                db.collection("talks").get().then((talks) => {
-                    var result = [[],[],[],[]];
-                    talks.forEach((t) => {
-                        var data = t.data();
-                        data["speakerObj"] = spks[data["speaker"]];
-                        if (data["day"] === "Monday 16th") {
-                            if(data["room"] === "Firebase Room") {
-                                result[0].push(data);
-                            } else if (data["room"] === "Secondary") {
-                                result[1].push(data);
-                            } else {
-                                result[0].push(data);
-                                result[1].push(data);
-                            }
-                        } else {
-                            if(data["room"] === "Firebase Room") {
-                                result[2].push(data);
-                            } else if (data["room"] === "Secondary") {
-                                result[3].push(data);
-                            } else {
-                                result[2].push(data);
-                                result[3].push(data);
-                            }
-                        }
-                    });
+//         }
+//     }
+// });
 
-                    var compare = function(a, b) {
-                        if (a["time"] > b["time"]) {
-                            return 1;
-                        } else if (a["time"] < b["time"]) {
-                            return -1;
-                        } else {
-                            return 0;
-                        }
-                    };
+// new Vue({
+//     el: '#schedule',
+//     data: {
+//         talks: [],
+//         speakers: {}
+//     },
+//     mounted: function() {
+//         this.loadData();
+//     },
+//     methods: {
+//         loadData: function() {
+//             var self = this;
 
-                    result[0].sort(compare);
-                    result[1].sort(compare);
-                    result[2].sort(compare);
-                    result[3].sort(compare);
-                    console.log(result);
-                    self.talks = result;
-                });
-            });
+//             db.collection("speakers").get().then((speakers) => {
+//                 var spks = {};
+//                 speakers.forEach((s) => {
+//                     var data = s.data();
+//                     spks[data["speakerId"]] = data;
+//                 });
+//                 self.speakers = spks;
 
-        }
-    }
-});
+//                 db.collection("talks").get().then((talks) => {
+//                     var result = [[],[],[],[]];
+//                     talks.forEach((t) => {
+//                         var data = t.data();
+//                         data["speakerObj"] = spks[data["speaker"]];
+//                         if (data["day"] === "Monday 16th") {
+//                             if(data["room"] === "Firebase Room") {
+//                                 result[0].push(data);
+//                             } else if (data["room"] === "Secondary") {
+//                                 result[1].push(data);
+//                             } else {
+//                                 result[0].push(data);
+//                                 result[1].push(data);
+//                             }
+//                         } else {
+//                             if(data["room"] === "Firebase Room") {
+//                                 result[2].push(data);
+//                             } else if (data["room"] === "Secondary") {
+//                                 result[3].push(data);
+//                             } else {
+//                                 result[2].push(data);
+//                                 result[3].push(data);
+//                             }
+//                         }
+//                     });
+
+//                     var compare = function(a, b) {
+//                         if (a["time"] > b["time"]) {
+//                             return 1;
+//                         } else if (a["time"] < b["time"]) {
+//                             return -1;
+//                         } else {
+//                             return 0;
+//                         }
+//                     };
+
+//                     result[0].sort(compare);
+//                     result[1].sort(compare);
+//                     result[2].sort(compare);
+//                     result[3].sort(compare);
+//                     console.log(result);
+//                     self.talks = result;
+//                 });
+//             });
+
+//         }
+//     }
+// });
